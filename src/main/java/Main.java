a8f6e19ec07ee41befac505f42b2d59064a101b4
Ivan <ivan.artishevski@codex-soft.com>
2016-03-18 14:39:50 +0300

    fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,12 @@ import com.artsisheuski.expenses.model.ExpenseDao;
 public class Main {
 
 	public static void main(String[] args) {
-		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
-		ctx.load("classpath:datasource-context.xml");
-		ctx.refresh();
-		ExpenseDao dao = (ExpenseDao) ctx.getBean("expenseDao");
-		System.out.println(dao.getExpenses());
-		System.out.println(dao.getExpense(1L));
+//		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+//		ctx.load("classpath:datasource-context.xml");
+//		ctx.refresh();
+//		ExpenseDao dao = (ExpenseDao) ctx.getBean("expenseDao");
+//		System.out.println(dao.getExpenses());
+//		System.out.println(dao.getExpense(1L));
 
 	}
 
